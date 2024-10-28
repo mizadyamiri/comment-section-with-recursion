a13dc0c28da95aeedf7ffa55a56a36b3ad92e293
mojtabaIzadyAmiri <mojtabaizady99@gmail.com>
2024-10-28 17:27:56 +0330

    feat: add product component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import "./App.css";
+import Product from "./components/Product";
 
 function App() {
-  return <div></div>;
+  return (
+    <main className="max-w-7xl mx-auto p-8">
+      <Product />
+    </main>
+  );
 }
 
 export default App;
